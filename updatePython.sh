dd2a3c7df3fef4b92a6522839ffc2aa0eac50e56
saladbowl77 <sarakun.info@gmail.com>
2022-03-22 21:39:13 +0900

    fix : branch name

--- updatePython.sh
+++ updatePython.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /mnt/sda1/$1
 git fetch origin
-git reset --hard origin/main
+git reset --hard origin/master
 #pip3 install -r requirements.txt
 python3 $2 gitTest
 if [ $? -gt 0 ]; then
