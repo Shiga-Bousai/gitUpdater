28a2e64d558d4945edd74d7aabcf85e3ea5724e0
saladbowl77 <sarakun.info@gmail.com>
2022-03-22 18:46:49 +0900

    add : error process

--- updatePython.sh
+++ updatePython.sh
@@ -2,5 +2,8 @@
 cd /mnt/sda1/$1
 git fetch origin
 git reset --hard origin/main
-pip3 install -r requirements.txt
-python3 $2
+#pip3 install -r requirements.txt
+python3 $2 gitTest
+if [ $? -gt 0 ]; then
+    git checkout @^
+fi
