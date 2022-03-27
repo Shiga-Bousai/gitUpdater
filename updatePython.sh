ac8ce0799153a0c340c4bf44a07a253e233422fd
saladbowl77 <sarakun.info@gmail.com>
2022-03-27 17:05:40 +0900

    fix : standard error → standard output

--- updatePython.sh
+++ updatePython.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 cd /mnt/sda1/$1
 echo "-----Git fetch-----"
-git fetch origin
-git reset --hard origin/master
+git fetch origin 2>&1
+git reset --hard origin/master 2>&1
 echo "-----pip3 install-----"
 pip3 install -r requirements.txt
 if [ $2 != "" ]; then
     echo "-----code test-----"
-    python3 $2 gitTest
+    python3 $2 gitTest 2>&1
     if [ $? -gt 0 ]; then
         echo "-----ERROR-----"
-        git stash
-        git checkout @^
-        git stash pop
+        git stash 2>&1
+        git checkout @^ 2>&1
+        git stash pop 2>&1
+    else
+        echo "-----Code is OK, No Problem-----"
     fi
 fi
 echo "-----Finished-----"
