6a82b4268cc96a6d92694b277ae111de70e58c19
saladbowl77 <sarakun.info@gmail.com>
2022-03-27 13:58:05 +0900

    fix : test running code

--- updatePython.sh
+++ updatePython.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 cd /mnt/sda1/$1
+echo "-----Git fetch-----"
 git fetch origin
 git reset --hard origin/master
-#pip3 install -r requirements.txt
-python3 $2 gitTest
-if [ $? -gt 0 ]; then
-    git stash
-    git checkout @^
-    git stash pop
+echo "-----pip3 install-----"
+pip3 install -r requirements.txt
+if [ $2 != "" ]; then
+    echo "-----code test-----"
+    python3 $2 gitTest
+    if [ $? -gt 0 ]; then
+        echo "-----ERROR-----"
+        git stash
+        git checkout @^
+        git stash pop
+    fi
 fi
+echo "-----Finished-----"
